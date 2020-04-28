1232f0618077c3c18019730e18d8e9c6be5fa68a
Michelle Lu <lu.michelle96@icloud.com>
2020-04-28 01:13:57 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,26 @@
 # Create your haunted_houses migration here
+
+  #1. name
+  #2. location
+  #3. theme
+  #4. price
+  #5. whether they're family friendly or not
+  #6. opening date
+  #7. closing date
+  #8. long description
+
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.timestamp :opening_date
+      t.timestamp :closing_date
+      t.text :description
+    end
+  end
+end
